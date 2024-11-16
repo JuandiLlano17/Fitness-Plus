f659894437924d0077ac41233c335bfc72512e42
Juan David Llano <est.juan.llano@unimilitar.edu.co>
2024-11-16 16:57:47 -0500

    Carpeta JS y referenciar header y footer en script de cada HTML

---
+++ js/Carga_header_footer.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+   // Cargar el header desde header.html
+   $('header').load('header.html');
+   // Cargar el footer desde footer.html
+   $('footer').load('footer.html');
+ });
